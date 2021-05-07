6bc0d94a6248282c0c33f59efdf918681ee9259b
Sudhamani544 <52409714+Sudhamani544@users.noreply.github.com>
2021-05-07 15:30:40 +0200

    Update models.py

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -14,27 +14,3 @@ class FoodItems(models.Model):
     slug  = models.SlugField()
     def __str__(self):
         return self.name
-
-    # def get_absolute_url(self):
-    #     return reverse("store:product",kwargs={
-    #     'slug': self.slug
-    #     })
-#
-# class OrdItem(models.Model):
-# #     # user= models.ForeignKey(User,on_delete=models.CASCADE)
-# #     # ordered = models.BooleanField(default = False)
-#     fooditem = models.ForeignKey(FoodItems,on_delete=models.CASCADE)
-# #     # quantity = models.IntegerField(default=1)
-
-#     def __str__(self):
-#         return 'f"{self.quantity} of {self.item.name}"'
-#
-# class Order(models.Model):
-#     user= models.ForeignKey(User,on_delete=models.CASCADE)
-#     items=models.ManyToManyField(OrderItem)
-#     start_date = models.DateTimeField(auto_now_add = True )
-#     ordered_date = models.DateTimeField()
-#     ordered = models.BooleanField(default = False)
-#
-#     def __str__():
-#         return self.user.username
