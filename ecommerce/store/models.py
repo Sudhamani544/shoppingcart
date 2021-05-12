c8fb4b442f07bbe688c7d6c7be831cd4a0bc82c5
Sudhamani544 <52409714+Sudhamani544@users.noreply.github.com>
2021-05-12 15:56:11 +0200

    Update models.py

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -3,6 +3,14 @@ from cloudinary.models import CloudinaryField
 from django.shortcuts import reverse
 
 # Create your models here.
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
+    name=models.CharField(max_length=200, null=True)
+    email=models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
 class FoodItems(models.Model):
     name  = models.CharField(max_length=100)
     image = CloudinaryField('image')
@@ -12,5 +20,57 @@ class FoodItems(models.Model):
     offer = models.BooleanField(default=False)
     discountprice = models.IntegerField(blank=True, null=True)
     slug  = models.SlugField()
+    
     def __str__(self):
         return self.name
+    # @property
+    # def imageUrl(self):
+    #     try:
+    #         url=self.image.url
+    #     except:
+    #         url=''
+    #     return url
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        print(self.orderitem_set.all())
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+class OrderItem(models.Model):
+    fooditem = models.ForeignKey(FoodItems,on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, default=True, null=True)
+    quantity = models.IntegerField(default=0, blank=True, null=True)
+    dateadded = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.fooditem.price * self.quantity
+        return total
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.address
