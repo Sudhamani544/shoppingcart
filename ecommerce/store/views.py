ed8e26b03447dc1bc12243f0771244675ecf052e
Sudhamani544 <52409714+Sudhamani544@users.noreply.github.com>
2021-05-12 15:57:21 +0200

    Update views.py

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import FoodItems
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from .models import *
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView
 # Create your views here.
 
@@ -8,9 +9,28 @@ class StoreView(ListView):
     template_name = "store/store.html"
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items=order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_items':0,'get_cart_total':0}
+    context = {'items':items, 'order':order}
     return render(request,'store/cart.html',context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items=order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_items':0,'get_cart_total':0}
+    context = {'items':items, 'order':order}
     return render(request,'store/checkout.html',context)
+
+# def store(request):
+#     items = FoodItems.objects.all()
+#     return render(request,'store/store.html', {'items' : items})
+
