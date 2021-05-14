41675936fda4fc27516240334e7aacf27000c737
Sudhamani544 <52409714+Sudhamani544@users.noreply.github.com>
2021-05-14 14:25:51 +0200

    Update models.py

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -42,23 +42,6 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
-    # @property
-    # def imageUrl(self):
-    #     try:
-    #         url=self.image.url
-    #     except:
-    #         url=''
-    #     return url
-
-class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False, null=True, blank=False)
-    transaction_id = models.CharField(max_length=200, null=True)
-
-    def __str__(self):
-        return str(self.id)
-
     @property
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
